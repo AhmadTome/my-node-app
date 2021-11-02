65e7cb02a39c1032134513a213223899c8411315
AhmadTome <atome@asaltech.com>
2021-11-02 10:28:26 +0200

    fix DB string

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,8 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 const mongoose = require('mongoose');
-const URI = process.env["APP_MONGO_BD_URL"];
+//const URI = process.env["APP_MONGO_BD_URL"];
+const URI = 'mongodb+srv://root:root@cluster0.dcgkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const connectDB = async () => {
     await mongoose.connect(URI, {
